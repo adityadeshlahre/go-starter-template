09dfe55ca0e9c35e5ae9374e4a10df2b00fcd571
Aditya Deshlahre <wymaditya@gmail.com>
2025-09-05 19:48:24 +0530

    feat: middleware + server + lib/job handlers added

--- apps/server/internal/lib/job/job.go
+++ apps/server/internal/lib/job/job.go
@@ -1,9 +1,9 @@
 package job
 
 import (
+	"github.com/adityadeshlahre/go-starter-template/internal/config"
 	"github.com/hibiken/asynq"
 	"github.com/rs/zerolog"
-	"github.com/sriniously/go-boilerplate/internal/config"
 )
 
 type JobService struct {
